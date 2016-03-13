3047d76c1a5ac084be7a0ef574cd4265757502a8
wduntak <wduntak@gmail.com>
2016-03-12 22:50:00 -0500

    Add context to routes and differentiate playerOne and playerTwo links

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,16 +2,50 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function () {
+		return {
+			username: ''
+		}
+	},
+	onUpdateUser: function(e) {
+		this.setState({
+			username: e.target.value
+		});
+	},
+	onSubmitUser: function (e) {
+		e.preventDefault();
+		var username = this.state.username;
+		this.setState({
+			username: ''
+		});
+
+		if (this.props.routeParams.playerOne) {
+			this.context.router.push({
+				pathname: '/battle',
+				query: {
+					playerOne: this.props.routeParams.playerOne,
+					playerTwo: this.state.username
+				}
+			})
+		} else {
+			this.context.router.push('/playerTwo/' + this.state.username)
+		}
+	},
 	render: function () {
 		return (
 			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
 				<h1>{this.props.route.header}</h1>
 				<div className="col-sm-12">
-					<form>
+					<form onSubmit={this.onSubmitUser}>
 						<div className="form-group">
 							<input
 								className="form-control"
 								placeholder="Github Username"
+								onChange={this.onUpdateUser}
+								value={this.state.username}
 								type="text" />
 						</div>
 						<div className="form-group col-sm-4 col-sm-offset-4">
