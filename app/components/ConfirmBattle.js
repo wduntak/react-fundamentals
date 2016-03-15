421df4aed7df8eb5bd220af56429a48fc34f71e5
wduntak <wduntak@gmail.com>
2016-03-14 23:06:54 -0400

    Add UI to the Battle page with player data seperated

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
 
 function puke (object) {
 	console.log(object);
@@ -8,7 +11,38 @@ function puke (object) {
 function ConfirmBattle (props) {
 	return props.isLoading === true
 		? <p> Loading! </p>
-		: <div> Confirm Battle!: {puke(props)}</div>
+		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+			<h1>Confirm Players</h1>
+			<div className="col-sm-8 col-sm-offset-2">
+				<div className="col-sm-6">
+					<p className="lead">Player 1</p>
+					{puke(props.playersInfo[0])}
+				</div>
+				<div className="col-sm-6">
+					<p className="lead">Player 2</p>
+					{puke(props.playersInfo[1])}
+				</div>
+			</div>
+			<div className="col-sm-8 col-sm-offset-2">
+				<div className="col-sm-12" style={styles.space}>
+					<button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+						Initiate Battle!
+					</button>
+				</div>
+				<div className="col-sm-12" style={styles.space}>
+					<Link to="/playerOne">
+						<button type="button" className="btn btn-lg btn-danger"> Reselect Players</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+
+}
+
+ConfirmBattle.propTypes = {
+	isLoading: PropTypes.bool.isRequired,
+	onInitiateBattle: PropTypes.func.isRequired,
+	playersInfo: PropTypes.array.isRequired
 }
 
 module.exports = ConfirmBattle;
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -9,7 +9,7 @@ var ConfirmBattleContainer = React.createClass({
 	getInitialState: function () {
 		return {
 			isLoading: true,
-			playerInfo: []
+			playersInfo: []
 		}
 	},
 	componentDidMount: function () {
@@ -23,10 +23,19 @@ var ConfirmBattleContainer = React.createClass({
 				})
 			}.bind(this))
 	},
+	handleInitiateBattle: function () {
+		this.context.router.push({
+			pathname: '/results',
+			state: {
+				playersInfo: this.state.playersInfo
+			}
+		})
+	},
 	render: function () {
 		return (
 			<ConfirmBattle 
 			isLoading={this.state.isLoading}
+			onInitiateBattle={this.handleInitiateBattle}
 			playersInfo={this.state.playersInfo} />
 		)
 	}
