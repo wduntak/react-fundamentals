dd716d722f4e48c0ad1f3d54910df884b4bf1a4b
wduntak <wduntak@gmail.com>
2016-03-16 23:21:12 -0400

    Refactor to add wrappers and fixed bug

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,16 +4,12 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-function puke (object) {
-	console.log(object);
-	return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var MainContainer = require('./MainContainer');
 
 function ConfirmBattle (props) {
 	return props.isLoading === true
 		? <p> Loading! </p>
-		: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+		: <MainContainer>
 			<h1>Confirm Players</h1>
 			<div className="col-sm-8 col-sm-offset-2">
 				<UserDetailsWrapper header='Player One'>
@@ -35,7 +31,7 @@ function ConfirmBattle (props) {
 					</Link>
 				</div>
 			</div>
-		</div>
+		</MainContainer>
 
 }
 
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,19 +1,19 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
 var ReactRouter = require('react-router');
-var Link = ReactRouter.Link;
+var Link = require('react-router').Link;
+var MainContainer = require('./MainContainer');
 
 
 var Home = React.createClass({
 	render: function () {
 		return (
-			<div className='jumbotron col-sm-12 text-center' style={transparentBg}>
+			<MainContainer>
 				<h1>Github Battle</h1>
 				<p className='lead'>Some fancy motto</p>
 				<Link to='/playerOne'>
 					<button type='button' className='btn btn-lg btn-success'>Get Started</button>
 				</Link>
-			</div>
+			</MainContainer>
 		)
 	}
 });
